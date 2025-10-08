d9d6f3f7844e510b972c23ace5e3a8ab905cf6eb
Gaétan Ulysse <gaet.ulysse@gmail.com>
2025-10-08 14:35:54 +0200

    suppression mapping 2

--- fonctions_traitement_dataframes.py
+++ fonctions_traitement_dataframes.py
@@ -48,42 +48,7 @@ column_mapping = {
     # Ajoute ici les autres mappings si tu as d'autres taches ou moyennes à intégrer
 }
 
-mapping2 = {
-    "Grass pure CFT  CS07": "Mean_Grass",
-    "Fluid make-up CFT CS17": "Mean_Fluid_Make_Up",
-    "Soot / Mineral oil CFT C01s": "Mean_ Soot_ Mineral_Oil",
-    "Bilberry juice CFT CS15": "Mean_Bilberry_Juice",
-    "Chocolate drink pure CFT CS44": "Mean_Chocolate_Drink_Pure",
-    "Oatmeal with chocolate CFT CS54": "Mean_Oatmeal_with_Chocolate",
-    "Salad dressing CFT CS06": "Mean_ Salad_Dressing",
-    "Rice starch CFT CS28": "Mean_Rice_Starch",
-    "Lard CFT CS62": "Mean_Lard",
-    "Red wine Equest WE5RWWKC": "Mean_Red_Wine",
-    "Napolina tomato puree Equest WE5TPWKC": "Mean_Napolina_Tomato_Puree",
-    "Cooked beef fat Equest WE5BBPC2": "Mean_Cooked_Beef_Fat",
-    "Mustard Equest WE5FSMWKC": "Mean_Mustard",
-    "Grass & mud Equest WE5GMWKC": "Mean_Grass&Mud",
-    "Sheep's blood Equest WE5DASBWKC": "Mean_Sheeps_blood",
-    "Tea Equest WE5LTWKC": "Mean-Tea",
-    "Coffee WE5ECWKC": "Mean_Coffee",
-    "Sebum & Carbon Black Equest- 146 KC": "Mean_Sebum",
-    #"Lipstick CFT CH078": "Mean_Lipstick",  # à vérifier si la colonne existe, sinon à ajouter
-    #"Green Pesto 1 - Equest 151 KC": "Mean_Green_Pesto",  # à vérifier si la colonne existe, sinon à ajouter
-    #"Frying fat CFT CS46B": "Mean_Frying_Fat",  # à vérifier si la colonne existe, sinon à ajouter
-    #"Dirty Motor oil  Equest 049 KC": "Mean_Dirty_Motor_Oil",  # à vérifier si la colonne existe, sinon à ajouter
-    #"Face MakeUp 1 Equest- 144 KC": "Mean_Face_MakeUp_1",  # à vérifier si la colonne existe, sinon à ajouter
-    "Soot / Mineral oil CFT C01s": "Mean_ Soot_ Mineral_Oil",
-    #"Greasy & particulate stain removal average": "Mean_Greasy_Particulate_Stain_Removal_Avg",  # à vérifier si la colonne existe, sinon à ajouter
-    #"Enzymatic stain removal average": "Mean_Enzymatic_Stain_Removal_Avg",  # à vérifier si la colonne existe, sinon à ajouter
-    #"Oxydable stains removal stain": "Mean_Oxydable_Stains_Removal",  # à vérifier si la colonne existe, sinon à ajouter
-    #"Average of Soil removal": "Mean_Soil_Removal_Avg",  # à vérifier si la colonne existe, sinon à ajouter
-    "Anti-greying Cotton WFK10A": "Mean_Anti_greying_Cotton_WFK10A",
-    "Anti-greying Polyester / Cotton WFK20A": "Mean_Anti_greying_Polyester_Cotton_WFK20A",
-    "Anti-greying Polyester WFK30A": "Mean_Anti_greying_Polyester_WFK30A",
-    "Anti-greying Poyamide WFK40A": "Mean_Anti_greying_Poyamide_WFK40A",
-    "Average of Anti-greying (Y)": "Mean_Anti_greying_Y"
-    # Ajoute ici les autres mappings si tu as d'autres taches ou moyennes à intégrer
-}
+
 
 def append_with_mapping(source_df, target_df, column_mapping):
     # 1. Renommer les colonnes du DataFrame source selon le mapping
